9e2bf142015ae0f8c261a53b92808adfa8ab52da
Yevgeniy <yevgeniy.engineer@gmail.com>
2023-07-16 23:42:05 +0800

    Added spherical grid

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
-#include "quad_sphere_tree.hpp"
-
+#include "spherical_grid.hpp"
 #include <algorithm>
 #include <chrono>
 #include <cstdlib>
@@ -8,23 +7,23 @@
 #include <limits>
 #include <vector>
 
-#define PI 3.14159265358979323846
+#define PI 3.14159265358979323846f
 #define NUM_POINTS 100000
 
 using namespace spatial_index;
 
-QuadSpherePoint randomPoint()
+SphericalPoint randomPoint()
 {
-    return QuadSpherePoint{
-        static_cast<double>(std::rand()) / RAND_MAX,              // radius_m in [0, 1]
-        static_cast<double>(std::rand()) / RAND_MAX * 2 * PI,     // azimuth_rad in [0, 2*pi].
-        static_cast<double>(std::rand()) / RAND_MAX * PI - PI / 2 // elevation_rad in [-pi/2, pi/2].
+    return SphericalPoint{
+        static_cast<float>(std::rand()) / RAND_MAX,              // radius_m in [0, 1]
+        static_cast<float>(std::rand()) / RAND_MAX * 2 * PI,     // azimuth_rad in [0, 2*pi].
+        static_cast<float>(std::rand()) / RAND_MAX * PI - PI / 2 // elevation_rad in [-pi/2, pi/2].
     };
 }
 
-bool operator==(const QuadSpherePoint &p1, const QuadSpherePoint &p2)
+bool operator==(const SphericalPoint &p1, const SphericalPoint &p2)
 {
-    static constexpr double tolerance = 1e-6;
+    static constexpr float tolerance = 1e-5f;
     return (std::abs(p1.radius_m - p2.radius_m) < tolerance) &&
            (std::abs(p1.azimuth_rad - p2.azimuth_rad) < tolerance) &&
            (std::abs(p1.elevation_rad - p2.elevation_rad) < tolerance);
@@ -35,56 +34,72 @@ int main()
     // Seed the random number generator.
     std::srand(std::time(nullptr));
 
-    // Create a tree.
-    QuadSphereTree tree(NUM_POINTS);
+    // Create a grid.
+    SphericalGrid grid(0.1f, 0.2f, 0.2f);
 
     // Create a vector to hold the points for brute force search.
-    std::vector<QuadSpherePoint> points;
-
-    // Generate random points and insert them into the tree and the vector.
+    std::vector<SphericalPoint> points;
     for (std::size_t i = 0; i < NUM_POINTS; ++i)
     {
-        QuadSpherePoint point = randomPoint();
-        tree.insert(point);
+        SphericalPoint point = randomPoint();
         points.push_back(point);
     }
 
+    // Generate random points and insert them into the grid and the vector.
+    auto t00 = std::chrono::high_resolution_clock::now();
+    for (const auto &point : points)
+    {
+        grid.insert(point);
+    }
+    auto t01 = std::chrono::high_resolution_clock::now();
+    std::cout << "Elapsed time (grid insertion, seconds): " << static_cast<double>((t01 - t00).count()) * 1e-9
+              << std::endl;
+
     // Generate a random target point.
-    QuadSpherePoint target = randomPoint();
+    SphericalPoint target = randomPoint();
 
-    // Search for the nearest neighbor using the tree.
+    // Search for the nearest neighbor using the grid.
     auto t1 = std::chrono::high_resolution_clock::now();
-
-    QuadSpherePoint nearest_tree = tree.nearestNeighbourSearch(target);
-
+    SphericalPointWithDistance nearest_point;
+    bool success = grid.getNearestNeighbor(target, nearest_point);
     auto t2 = std::chrono::high_resolution_clock::now();
 
     // Search for the nearest neighbor using brute force.
-    QuadSpherePoint nearest_brute;
-    double min_distance = std::numeric_limits<double>::max();
-    for (const QuadSpherePoint &point : points)
+    SphericalPoint nearest_brute;
+    float min_distance = std::numeric_limits<float>::max();
+    for (const SphericalPoint &point : points)
     {
-        double distance = tree.distance(point, target);
+        float distance = grid.getDistance(point, target);
         if (distance < min_distance)
         {
             min_distance = distance;
             nearest_brute = point;
         }
     }
-
     auto t3 = std::chrono::high_resolution_clock::now();
 
+    if (!success)
+    {
+        std::cout << "Could not find nearest neighbour using grid\n";
+    }
+
     // Check that the results are the same.
-    if (nearest_tree == nearest_brute)
+    if (nearest_point.point == nearest_brute)
     {
         std::cout << "Test passed!" << std::endl;
     }
     else
     {
         std::cout << "Test failed!" << std::endl;
+        std::cout << "Grid NN: radius_m = " << nearest_point.point.radius_m
+                  << ", azimuth_rad = " << nearest_point.point.azimuth_rad
+                  << ", elevation_rad = " << nearest_point.point.elevation_rad << std::endl;
+        std::cout << "Brute-force NN: radius_m = " << nearest_brute.radius_m
+                  << ", azimuth_rad = " << nearest_brute.azimuth_rad
+                  << ", elevation_rad = " << nearest_brute.elevation_rad << std::endl;
     }
 
-    std::cout << "Elapsed time (tree search, seconds): " << static_cast<double>((t2 - t1).count()) * 1e-9 << std::endl;
+    std::cout << "Elapsed time (grid search, seconds): " << static_cast<double>((t2 - t1).count()) * 1e-9 << std::endl;
     std::cout << "Elapsed time (brute force search, seconds): " << static_cast<double>((t3 - t2).count()) * 1e-9
               << std::endl;
 
---
+++ spherical_grid.hpp
@@ -0,0 +1,252 @@
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <queue>
+#include <stdexcept>
+#include <vector>
+
+namespace spatial_index
+{
+struct SphericalPoint
+{
+    float radius_m;
+    float azimuth_rad;
+    float elevation_rad;
+};
+
+struct SphericalPointWithDistance
+{
+    SphericalPoint point;
+    float distance;
+};
+
+template <std::size_t CellCapacity = 10000U> struct SphericalCell
+{
+    static constexpr auto CELL_CAPACITY = CellCapacity;
+
+    SphericalCell() : points{new SphericalPoint[CELL_CAPACITY]}, count(0U)
+    {
+    }
+
+    ~SphericalCell()
+    {
+        delete[] points;
+    }
+
+    void reset()
+    {
+        count = 0U;
+    }
+
+    SphericalPoint *points;
+    std::size_t count;
+};
+
+template <std::size_t CellCapacity = 10000U> class SphericalGrid
+{
+    static constexpr float TWO_PI = 2.0f * M_PIf;
+    static constexpr float ONE_PI = M_PIf;
+    static constexpr float HALF_PI = 0.5f * M_PIf;
+
+  public:
+    static constexpr auto CELL_CAPACITY = CellCapacity;
+
+    explicit SphericalGrid(float radial_cell_size_m, float azimuthal_cell_size_rad, float elevation_cell_size_rad)
+        : radial_cell_size_m_{radial_cell_size_m}, azimuthal_cell_size_rad_{azimuthal_cell_size_rad},
+          elevation_cell_size_rad_{elevation_cell_size_rad}
+    {
+        radial_divisions_ = 1000U; // How to choose this?
+        azimuthal_divisions_ = std::ceil(TWO_PI / azimuthal_cell_size_rad_);
+        elevation_divisions_ = std::ceil(ONE_PI / elevation_cell_size_rad_);
+        angular_divisions_ = azimuthal_divisions_ * elevation_divisions_;
+
+        total_cells_ = radial_divisions_ * azimuthal_divisions_ * elevation_divisions_;
+        spherical_grid_ = new SphericalCell<CELL_CAPACITY>[total_cells_];
+    }
+
+    ~SphericalGrid()
+    {
+        delete[] spherical_grid_;
+    }
+
+    void reset()
+    {
+        for (std::size_t i = 0U; i < total_cells_; ++i)
+        {
+            spherical_grid_->reset();
+        }
+    }
+
+    inline std::size_t cellIndex(const SphericalPoint &point) noexcept
+    {
+        std::size_t radial_cell_index = static_cast<std::size_t>(point.radius_m / radial_cell_size_m_);
+
+        // Wrap azimuthal angle to [0, 2*PI]
+        float wrapped_azimuth = std::fmod(point.azimuth_rad, TWO_PI);
+        if (wrapped_azimuth < 0.0f)
+        {
+            wrapped_azimuth += TWO_PI;
+        }
+        std::size_t azimuthal_cell_index = static_cast<std::size_t>(wrapped_azimuth / azimuthal_cell_size_rad_);
+
+        // Wrap elevation angle to [0, PI]
+        float wrapped_elevation = std::fmod(point.elevation_rad, ONE_PI);
+        if (wrapped_elevation < 0.0f)
+        {
+            wrapped_elevation += ONE_PI;
+        }
+        std::size_t elevation_cell_index = static_cast<std::size_t>(wrapped_elevation / elevation_cell_size_rad_);
+
+        std::size_t cell_index =
+            radial_cell_index * angular_divisions_ + elevation_cell_index * azimuthal_divisions_ + azimuthal_cell_index;
+
+        return cell_index;
+    }
+
+    inline void insert(const SphericalPoint &point)
+    {
+        std::size_t cell_index = cellIndex(point);
+        if (spherical_grid_[cell_index].count >= CELL_CAPACITY)
+        {
+            throw std::runtime_error("Exceeded cell capacity");
+        }
+        spherical_grid_[cell_index].points[spherical_grid_[cell_index].count++] = point;
+    }
+
+    inline SphericalCell<CELL_CAPACITY> &getCell(const SphericalPoint &point)
+    {
+        std::size_t cell_index = cellIndex(point);
+        return spherical_grid_[cell_index];
+    }
+
+    struct Haversine
+    {
+        inline float operator()(float value) const noexcept
+        {
+            return std::pow(std::sin(value / 2.0f), 2.0f);
+        }
+    };
+
+    inline static float getDistance(const SphericalPoint &p1, const SphericalPoint &p2)
+    {
+        // See https://math.stackexchange.com/questions/833002/distance-between-two-points-in-spherical-coordinates
+
+        // static constexpr Haversine haversine{};
+
+        // float d_azimuth_rad = p2.azimuth_rad - p1.azimuth_rad;
+        // float d_elevation_rad = p2.elevation_rad - p1.elevation_rad;
+
+        // float a = haversine(d_elevation_rad) + std::cos(p1.elevation_rad) * std::cos(p2.elevation_rad);
+        // float c = 2.0f * std::atan2(std::sqrt(a), std::sqrt(1.0f - a));
+
+        // return 0.5f * (p1.radius_m + p2.radius_m) * c;
+
+        float cos_angle =
+            std::sin(p1.elevation_rad) * std::sin(p2.elevation_rad) * std::cos(p1.azimuth_rad - p2.azimuth_rad) +
+            std::cos(p1.elevation_rad) * std::cos(p2.elevation_rad);
+
+        float distance_squared =
+            p1.radius_m * p1.radius_m + p2.radius_m * p2.radius_m - 2 * p1.radius_m * p2.radius_m * cos_angle;
+
+        return distance_squared;
+    }
+
+    inline bool getNearestNeighbor(const SphericalPoint &point, SphericalPointWithDistance &nearest_neighbour)
+    {
+        const SphericalCell<CELL_CAPACITY> &cell = getCell(point);
+
+        // Handle the case where the cell is empty
+        bool found_neighbour = false;
+        if (cell.count > 0)
+        {
+            nearest_neighbour.distance = getDistance(point, cell.points[0]);
+
+            std::size_t nearest_neighbour_index = 0U;
+            for (std::size_t i = 1; i < cell.count; ++i)
+            {
+                float distance = getDistance(point, cell.points[i]);
+                if (distance < nearest_neighbour.distance)
+                {
+                    nearest_neighbour_index = i;
+                    nearest_neighbour.distance = distance;
+                }
+            }
+
+            nearest_neighbour.point = cell.points[nearest_neighbour_index];
+            found_neighbour = true;
+        }
+
+        return found_neighbour;
+    }
+
+    struct CompareByDistance
+    {
+        inline bool operator()(const SphericalPoint &p, const SphericalPoint &p1, const SphericalPoint &p2)
+        {
+            return getDistance(p, p1) < getDistance(p, p2);
+        }
+    };
+
+    inline bool getKNearestNeighbours(const SphericalPoint &point, std::size_t K,
+                                      std::vector<SphericalPointWithDistance> &nearest_neighbours)
+    {
+        static constexpr CompareByDistance compare_by_distance{};
+
+        nearest_neighbours.clear();
+
+        std::size_t cell_index = cellIndex(point);
+        std::vector<bool> visited(total_cells_, false);
+        std::queue<std::size_t> to_visit;
+
+        to_visit.push(cell_index);
+        visited[cell_index] = true;
+
+        while (!to_visit.empty())
+        {
+            std::size_t current_cell_index = to_visit.front();
+            to_visit.pop();
+
+            const SphericalCell<CELL_CAPACITY> &cell = spherical_grid_[current_cell_index];
+            for (std::size_t i = 0; i < cell.count; ++i)
+            {
+                float d = distance(point, cell.points[i]);
+                nearest_neighbours.push_back({cell.points[i], d});
+            }
+
+            if (nearest_neighbours.size() >= K)
+            {
+                std::partial_sort(nearest_neighbours.begin(), nearest_neighbours.begin() + K, nearest_neighbours.end(),
+                                  compare_by_distance);
+                nearest_neighbours.resize(K);
+                return true;
+            }
+
+            for (std::size_t neighbor_index : cell.neighbors)
+            {
+                if (!visited[neighbor_index])
+                {
+                    to_visit.push(neighbor_index);
+                    visited[neighbor_index] = true;
+                }
+            }
+        }
+
+        // If we reach this point, it means there are less than K points in the entire grid
+        std::sort(nearest_neighbours.begin(), nearest_neighbours.end(), compare_by_distance);
+        return false;
+    }
+
+  private:
+    float radial_cell_size_m_;
+    float azimuthal_cell_size_rad_;
+    float elevation_cell_size_rad_;
+    std::size_t radial_divisions_;
+    std::size_t azimuthal_divisions_;
+    std::size_t elevation_divisions_;
+    std::size_t angular_divisions_;
+    std::size_t total_cells_;
+    SphericalCell<CELL_CAPACITY> *spherical_grid_;
+};
+} // namespace spatial_index
